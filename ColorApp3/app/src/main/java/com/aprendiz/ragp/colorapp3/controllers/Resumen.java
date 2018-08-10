7290196030561200048521907a274e4e78adb475
DavidPyn27 <popayan159@gmail.com>
2018-08-10 17:55:55 -0500

    commitgf

--- ColorApp3/app/src/main/java/com/aprendiz/ragp/colorapp3/controllers/Resumen.java
+++ ColorApp3/app/src/main/java/com/aprendiz/ragp/colorapp3/controllers/Resumen.java
@@ -1,7 +1,10 @@
 package com.aprendiz.ragp.colorapp3.controllers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.aprendiz.ragp.colorapp3.R;
 
@@ -11,5 +14,37 @@ public class Resumen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resumen);
+
+
+
+    }
+
+    public void salir(View view) {
+        finish();
+    }
+
+    public void twi(View view) {
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, "");
+        intent.setPackage("com.twitter.android");
+
+        try {
+            startActivity(intent);
+
+        }catch (Exception e){
+            Toast.makeText(this, "No cuentas con está aplicación", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+    }
+
+    public void face(View view) {
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, "");
+        intent.setPackage("com.facebook.katana");
+        startActivity(intent);
     }
 }
