b181fe7ae527be618749d9f2c9071ae079642bac
DavidPyn27 <popayan159@gmail.com>
2018-08-10 18:03:17 -0500

    commitxc

--- ColorApp3/app/src/main/java/com/aprendiz/ragp/colorapp3/controllers/MenuJ.java
+++ ColorApp3/app/src/main/java/com/aprendiz/ragp/colorapp3/controllers/MenuJ.java
@@ -3,6 +3,7 @@ package com.aprendiz.ragp.colorapp3.controllers;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 
 import com.aprendiz.ragp.colorapp3.R;
@@ -20,5 +21,18 @@ public class MenuJ extends AppCompatActivity {
         Intent intent = new Intent(MenuJ.this,Juego.class);
         guardar=1;
         startActivity(intent);
+    }
+
+
+    public void puntu(View view) {
+        Intent intent1 = new Intent(MenuJ.this, Puntuacion.class);
+        guardar =0;
+        startActivity(intent1);
+    }
+
+    public void confi(View view) {
+        Intent intent1 = new Intent(MenuJ.this, Configuracion.class);
+        guardar=0;
+        startActivity(intent1);
     }
 }
